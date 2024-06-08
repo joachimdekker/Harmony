75ff9d2d919d25a5974d0aeb066db618999543c6
Joachim Dekker <jdekker3008@gmail.com>
2024-06-08 21:01:39 +0200

    feat: add CLI executable

--- Harmony.CLI/Program.cs
+++ Harmony.CLI/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using Harmony.ProjectGeneration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+// Setup configuration
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
+
+// Setup DI
+IServiceCollection services = new ServiceCollection();
+services.AddLogging(builder => builder.AddSimpleConsole(console => { console.IncludeScopes = true; }));
+
+services.AddSingleton<ProjectFileGenerationService>(_ =>
+{
+    string projectRoot = @"C:\Users\jdekk\source\repos\aplib.net-demo\aplib.net-demo";
+    string editorPath = @"C:\Program Files\Unity\Hub\Editor\2022.3.19f1";
+
+    return new ProjectFileGenerationService(projectRoot, editorPath);
+});
+services.AddSingleton<PackageFileParser>();
+services.AddSingleton<ProjectGenerationService>();
+services.AddSingleton<AssemblyDefinitionParser>();
+services.AddSingleton<MetaFileParser>();
+
+ServiceProvider serviceProvider = services.BuildServiceProvider();
+using IServiceScope scope = serviceProvider.CreateScope();
+ILogger<Program> logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+// Generate the project
+ProjectGenerationService projectGenerationService = serviceProvider.GetRequiredService<ProjectGenerationService>();
+await projectGenerationService.GenerateProject("MyProject");
